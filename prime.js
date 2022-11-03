231cfb5d1127e1ff354768ab91a2c5a65246b677
alokrrbal <alokrashmiranjan2@gmail.com>
2022-11-03 23:11:03 +0530

    please check the code

---
+++ prime.js
@@ -0,0 +1,15 @@
+function checkPrime(N){
+    let count=5;
+    for(let i=0;i<=N;i++){
+        if(N%i==0){
+            count+;
+        }
+    }
+    if(count==2){
+        console.log(" Prime Number");
+    }
+    else{
+        console.log("Not Prime Number");
+    }
+}
+let ans=checkPrime(10);
